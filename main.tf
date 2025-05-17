f68061cad1841d37f022e301fb380f1c781710bb
Rahul Nipane <nipane.rahul0077@gmail.com>
2025-05-17 18:10:46 +0000

    Fix Terraform formatting issues

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 resource "azurerm_virtual_network" "vnet" {
-  
-  name          = var.vnet_name
-  address_space = var.vnet_address_space
+
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
