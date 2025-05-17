b21af63c8b239758fd402c557b0a963d632ccbda
Rahul Nipane <nipane.rahul0077@gmail.com>
2025-05-17 16:24:08 +0000

    Secured provider config using variables

--- variables.tf
+++ variables.tf
@@ -17,3 +17,10 @@ variable "vnet_address_space" {
   description = "Address space for VNet"
   type        = list(string)
 }
+
+variable "client_id" {}
+variable "client_secret" {
+  sensitive = true
+}
+variable "tenant_id" {}
+variable "subscription_id" {}
