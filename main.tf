900919222fdc9296350319e8387e61b63d6031e6
Rahul Nipane <nipane.rahul0077@gmail.com>
2025-05-17 17:19:39 +0000

    Initial Terraform infrastructure code

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-demo"
-  location = "Australia East"
-}
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform-demo"
-  address_space       = ["10.0.0.0/16"]
+  
+  name          = var.vnet_name
+  address_space = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
